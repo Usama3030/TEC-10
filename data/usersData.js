d8013e69dd918206a167a28e13819bf63a72199d
Usama <usamabajwa3030@gmail.com>
2023-08-12 20:35:24 +0500

    api changes

---
+++ data/usersData.js
@@ -0,0 +1,15 @@
+// data/usersData.js
+const users = [
+    { id: 1, name: 'John Doe', email: 'john@example.com' },
+    { id: 2, name: 'Jane Smith', email: 'jane@example.com' },
+    { id: 3, name: 'Jaan Smith', email: 'jane@example.com' },
+    { id: 4, name: 'John Smith', email: 'jane@example.com' },
+    { id: 5, name: 'Jane Smith', email: 'jane@example.com' },
+    { id: 7, name: 'Jane Smith', email: 'jane@example.com' },
+    { id: 8, name: 'Jane Smith', email: 'jane@example.com' },
+    { id: 9, name: 'Jane Smith', email: 'jane@example.com' },
+    // Add more users as needed
+  ];
+  
+  module.exports = users;
+  
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
+// routes/users.js
 var express = require('express');
 var router = express.Router();
+const usersData = require('../data/usersData'); // Import the users data
 
-/* GET users listing. */
+// API endpoint to get users with pagination, sorting, and record length
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  const page = parseInt(req.query.page) || 1;
+  const sort = req.query.sort || 'id';
+  const length = parseInt(req.query.length) || usersData.length;
+
+  const startIndex = (page - 1) * length;
+  const endIndex = startIndex + length;
+  const sortedUsers = usersData.slice().sort((a, b) => {
+    if (sort === 'name') {
+      return a.name.localeCompare(b.name);
+    }
+    return a[sort] - b[sort];
+  });
+
+  const selectedUsers = sortedUsers.slice(startIndex, endIndex);
+  res.json(selectedUsers);
 });
 
 module.exports = router;
